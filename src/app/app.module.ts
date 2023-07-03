1c52c370d35a722af70cb7dbb90d95f76ca2175d
JhanAntezana <jhan.antezana6174@gmail.com>
2023-07-02 23:20:52 -0500

    creacion de componentes iniciales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { ContactanosComponent } from './components/contactanos/contactanos.component';
+import { AreasComponent } from './components/areas/areas.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuBarComponent,
+    ContactanosComponent,
+    AreasComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
